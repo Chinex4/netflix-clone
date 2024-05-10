a93079b5ddae7615bb8750b87cb729e32f0f591c
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2024-05-10 14:33:44 +0100

    added the accordion effect

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const questions = document.querySelectorAll(".quest");
 
-questions.forEach((quest) => {
-  quest.addEventListener("click", () => {
-    const answers = document.querySelectorAll('.answers')
-    console.log(answers)
-    // if (!answer.classList.contains("show")) {
-    //   answer.classList.add("show");
-    // } else {
-    //   answer.classList.remove("show");
-    // }
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+    const symbol = question.querySelector("span");
+
+    const answers = document.querySelectorAll(".answer");
+
+    answers.forEach((ans) => {
+      ans.classList.remove("show");
+      ans.previousElementSibling.querySelector("span").textContent = "+";
+    });
+
+    if (!answer.classList.contains("show")) {
+      answer.classList.add("show");
+      symbol.textContent = "-";
+    } else {
+      answer.classList.remove("show");
+      symbol.textContent = "+";
+    }
   });
 });
