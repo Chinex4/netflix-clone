e080440f626d4b03b850ea66751e33fcf64a8ff3
Chinaza Okuefuna <79304687+Chinex4@users.noreply.github.com>
2024-05-13 10:53:35 +0100

    made some changes

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-const questions = document.querySelectorAll(".quest");
+const questions = document.querySelectorAll('.quest');
+const inputEmail = document.querySelectorAll('.email');
+inputEmail.forEach((emails) => {
+	emails.addEventListener('input', () => {
+		if (emails.value === '') {
+			emails.classList.remove('active');
+		} else {
+			emails.classList.add('active');
+		}
+	});
+});
 
 questions.forEach((question) => {
-  question.addEventListener("click", () => {
-    const answer = question.nextElementSibling;
-    const symbol = question.querySelector("span");
+	question.addEventListener('click', () => {
+		const answer = question.nextElementSibling;
+		const symbol = question.querySelector('span');
 
-    const answers = document.querySelectorAll(".answer");
+		const answers = document.querySelectorAll('.answer');
 
-    answers.forEach((ans) => {
-      ans.classList.remove("show");
-      ans.previousElementSibling.querySelector("span").textContent = "+";
-    });
+		answers.forEach((ans) => {
+			ans.classList.remove('show');
+			ans.previousElementSibling.querySelector('span').textContent = '+';
+		});
 
-    if (!answer.classList.contains("show")) {
-      answer.classList.add("show");
-      symbol.textContent = "-";
-    } else {
-      answer.classList.remove("show");
-      symbol.textContent = "+";
-    }
-  });
+		if (!answer.classList.contains('show')) {
+			answer.classList.add('show');
+			symbol.textContent = '-';
+		} else {
+			answer.classList.remove('show');
+			symbol.textContent = '+';
+		}
+	});
 });
+
+inputEmail;
